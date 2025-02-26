dd71e0cdf13eb22526f8e4591c1050dc0e65cc76
chasacademy-sandra-larsson <111293794+chasacademy-sandra-larsson@users.noreply.github.com>
2025-02-26 12:10:14 +0100

    lesson ended

--- src/index.ts
+++ src/index.ts
@@ -16,21 +16,113 @@ const pool = mysql.createPool({
   port: 8889 // Mac 8889, Windows 3306
 });
 
+type User = {
+  id: number;
+  username: string;
+  password: string;
+}
 
 
 // Helper function
 async function query(sql:string, params: any[]) {
        const [ result] = await pool.execute(sql, params)
-      return result;
+      return result as User[] ;
 }
 
 
-app.get("/", (req, res) => {
-  res.send("Node.js and Express.js with TypeScript");
+// Route - Skapa användare och lösenord i users-tabellen
+app.post("/users", async (req, res) => {
+
+  const { username, password } = req.body;
+
+  try {
+    const result = await query(
+      "INSERT INTO users (username, password) VALUES (?, ?)",
+      [username, password]
+    );
+
+    console.log(result);
+    res.status(201).send("User created")
+
+  } catch(error) {
+    console.log("Error creating user", error);
+    res.status(500).send("Error creating user");
+  }
+
 });
 
+// Login med användare och lösenord och se om det matchar i databasen
+app.post("/login", async (req, res) => {
+
+  const { username, password } = req.body;
+  try {
+
+    // Hämta användare i db
+    const result = await query(
+      "SELECT * FROM users WHERE username = ?",
+      [username]
+    )
+
+    const user = result[0];
+
+    if(user.password === password) {
+      res.status(200).send("Login successfull")
+    } else {
+      res.status(401).send("invalid username or password")
+    }
+
+  } catch(error) {
+
+  }
+});
+
+app.put("/new-password", async(req, res) => {
+
+  const {username, newPassword} = req.body;
+
+  try {
+
+    const result = await query(
+       "SELECT * FROM users WHERE username = ?",
+       [username]
+    )
+
+    const user = result[0];
+
+    const updatedResult = await query(
+      "UPDATE users SET password = ? WHERE id = ?",
+      [newPassword, user.id]
+    )
+
+    console.log("updatedResult", updatedResult);
+    res.status(204).send("User created");
 
 
+  } catch {
+    res.status(500).send("Error updating user")
+  }
+
+})
+
+// Ta bort en användare
+app.delete("/users", async(req, res) => {
+      const { username } = req.body;
+
+
+      try {
+
+        const result = await query(
+          "DELETE FROM users WHERE username = ?",
+            [username]
+        )
+
+        res.send("User deleted");
+
+      } catch(error) {
+        res.status(500).send("Error deleting  user")
+
+      }
+})
 
 
 
