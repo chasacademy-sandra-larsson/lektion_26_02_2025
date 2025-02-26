882b0a6fae99b44bb183fcd882814a34028379b7
chasacademy-sandra-larsson <111293794+chasacademy-sandra-larsson@users.noreply.github.com>
2025-02-26 11:20:42 +0100

    db connection

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,38 @@
 import express from "express";
 import cors from "cors";
+import mysql from 'mysql2/promise'
 
 const app = express();
 
 app.use(cors());
+app.use(express.json());
+
+// Uppkoppling mot databasen
+const pool = mysql.createPool({
+  host: "localhost",
+  user: "root",
+  password: "root",
+  database: "banksajt2025",
+  port: 8889 // Mac 8889, Windows 3306
+});
+
+
+
+// Helper function
+async function query(sql:string, params: any[]) {
+       const [ result] = await pool.execute(sql, params)
+      return result;
+}
+
 
 app.get("/", (req, res) => {
   res.send("Node.js and Express.js with TypeScript");
 });
+
+
+
+
+
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
